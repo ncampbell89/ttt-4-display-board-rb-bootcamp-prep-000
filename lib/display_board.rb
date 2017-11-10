1931c4b87d9fc26fad289b78634952ff01c58fc9
Nicole Campbell <nscampbell303@gmail.com>
2017-11-10 19:14:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   board = [" "," "," "," ","X"," "," "," "," "]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
 end
