f1f7d399ccca6e211e2d5311f2e92e2360f18158
Nicole Campbell <nscampbell303@gmail.com>
2017-11-10 19:42:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,10 @@
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
 
-board = [" "," "," "," ","X"," "," "," "," "]
-display_board(board)
+board[4] = "#{}"
+#[" ", " ", " ", " ", "X", " ", " ", " ", " "]
